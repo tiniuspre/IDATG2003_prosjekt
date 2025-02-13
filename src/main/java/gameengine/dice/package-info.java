078235b58b1edbb9256e901b6f3335e51789f39a
xyfuss <jonasaatomren@gmail.com>
2025-02-13 20:24:35 +0100

    Updated package info for dice package.

---
+++ src/main/java/gameengine/dice/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the classes related to the game dice implementation
+ * in the game engine. It includes the state and behavior of the dice.
+ */
+package gameengine.dice;
