e58f17a4aa81225cc482d034607b2ab1ca36c062
xyfuss <jonasaatomren@gmail.com>
2025-02-17 15:55:23 +0100

    Basic tile functionality for chutes and ladders

--- src/main/java/gameengine/board/Tile.java
+++ src/main/java/gameengine/board/Tile.java
@@ -1,5 +1,7 @@
 package gameengine.board;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * The {@code Tile} class represents the tiles of the board in the game engine.
  * It contains the state and behavior of the game tiles.
@@ -13,11 +15,51 @@ public abstract class Tile {
   /**
    * The number of the tile.
    */
-  private int number;
+  private final int number;
   /**
    * The action of the tile.
    */
   private TileAction action;
+  /**
+   * The x position of the tile.
+   */
+  private final int posX;
+  /**
+   * The y position of the tile.
+   */
+  private final int posY;
 
+  /**
+   * Constructs a new {@code Tile} object
+   * with the specified number, pos_x and pos_y.
+   *
+   * @param num the number of the tile.
+   * @param positionX the x position of the tile.
+   * @param positionY the y position of the tile.
+   */
+  public Tile(final int num, final int positionX, final int positionY) {
+    this.number = num;
+    this.posX = positionX;
+    this.posY = positionY;
+  }
 
+  public final int getNumber() {
+    return number;
+  }
+
+  public final TileAction getAction() {
+    return action;
+  }
+
+  public final void setAction(@NotNull final TileAction tileAction) {
+    this.action = tileAction;
+  }
+
+  public final int getPosX() {
+    return posX;
+  }
+
+  public final int getPosY() {
+    return posY;
+  }
 }
--- src/main/java/testgame/engine/TestGameTile.java
+++ src/main/java/testgame/engine/TestGameTile.java
@@ -1,4 +1,66 @@
 package testgame.engine;
 
-public class TestGameTile {
+import gameengine.board.Tile;
+
+/**
+ * The {@code TestGameTile} class represents the game tile in the test game.
+ */
+public class TestGameTile extends Tile {
+
+  /**
+   * Constructor for the TestGameTile class.
+   *
+   * @param number the number of the tile.
+   * @param posX the x position of the tile.
+   * @param posY the y position of the tile.
+   */
+  public TestGameTile(final int number, final int posX, final int posY) {
+    super(number, posX, posY);
+  }
+}
+
+class SnakeTile extends TestGameTile {
+
+  private int destinationX;
+  private int destinationY;
+
+  SnakeTile(final int number, final int posX, final int posY) {
+    super(number, posX, posY);
+  }
+
+  public void setDestination(final int positionX, final int positionY) {
+    this.destinationX = positionX;
+    this.destinationY = positionY;
+  }
+
+  public int[] getDestination() {
+    return new int[] {destinationX, destinationY};
+  }
+}
+
+class LadderTile extends TestGameTile {
+
+  private int destinationX;
+  private int destinationY;
+
+  LadderTile(final int number, final int posX, final int posY) {
+    super(number, posX, posY);
+  }
+
+  public void setDestination(final int positionX, final int positionY) {
+    this.destinationX = positionX;
+    this.destinationY = positionY;
+  }
+
+  public int[] getDestination() {
+    return new int[] {destinationX, destinationY};
+  }
+
+}
+
+class NormalTile extends TestGameTile {
+
+  NormalTile(final int number, final int posX, final int posY) {
+    super(number, posX, posY);
+  }
 }
