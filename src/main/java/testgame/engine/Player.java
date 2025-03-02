887c4270e6175ad8dfe10727757304053cb017c1
xyfuss <jonasaatomren@gmail.com>
2025-03-02 14:49:32 +0100

    Refactored according to checkstyle and added documentation.

--- src/main/java/testgame/engine/Player.java
+++ src/main/java/testgame/engine/Player.java
@@ -1,43 +1,93 @@
 package testgame.engine;
 
+/**
+ * The {@code Player} class represents a player in the test
+ * snakes and ladders game.
+ *
+ * @author jonastomren
+ * @version 13.02.2025
+ * @since 13.02.2025
+ */
 public class Player {
+  /**
+   * The name of the player.
+   */
   private String name;
+  /**
+   * The position of the player on the board.
+   */
   private int position = 0;
 
-  public Player(String name) {
-    setName(name);
+  /**
+   * Constructor for the Player class.
+   *
+   * @param inputName the name of the player.
+   */
+  public Player(final String inputName) {
+    setName(inputName);
   }
 
-  public void setName(String inputName) {
+  /**
+   * Sets the name of the player.
+   *
+   * @param inputName the name of the player.
+   * @throws IllegalArgumentException if the name is null or empty.
+   */
+  public void setName(final String inputName) throws IllegalArgumentException {
     if (inputName == null || inputName.isEmpty() || inputName.isBlank()) {
       throw new IllegalArgumentException("Name cannot be null or empty");
     }
     this.name = inputName;
   }
 
+  /**
+   * Returns the name of the player.
+   *
+   * @return the name of the player.
+   */
   public String getName() {
     return name;
   }
 
-  public void setPosition(int position) {
-    if (position < 0) {
-      throw new IllegalArgumentException("Invalid position");
+  /**
+   * Sets the inputPosition of the player on the board.
+   *
+   * @param inputPosition the inputPosition of the player.
+   */
+  public void setPosition(final int inputPosition) {
+    if (inputPosition < 0) {
+      throw new IllegalArgumentException("Invalid inputPosition");
     }
-    this.position = position;
+    this.position = inputPosition;
   }
 
+  /**
+   * Returns the position of the player on the board.
+   *
+   * @return the position of the player.
+   */
   public int getPosition() {
     return position;
   }
 
-  public void move(int steps) {
+  /**
+   * Moves the player forward a number of steps.
+   *
+   * @param steps the number of steps to move.
+   */
+  public void move(final int steps) {
     if (steps < 0) {
       throw new IllegalArgumentException("Invalid steps");
     }
     position += steps;
   }
 
-  public void moveBack(int tileNumber) {
+  /**
+   * Moves the player back to a specific tile.
+   *
+   * @param tileNumber the number of the tile to move back to.
+   */
+  public void moveBack(final int tileNumber) {
     setPosition(tileNumber);
   }
 }
--- src/main/java/testgame/engine/TestGameBoard.java
+++ src/main/java/testgame/engine/TestGameBoard.java
@@ -7,7 +7,13 @@ import testgame.engine.tiles.NormalTile;
 import testgame.engine.tiles.SnakeTile;
 
 /**
- * The {@code TestGameBoard} class represents the game board in the test game.
+ * The {@code TestGameBoard} class represents the game board in the test game,
+ * extending the {@code Board} class.
+ *
+ * @author jonastomren
+ * @version 13.02.2025
+ * @since 13.02.2025
+ * @see Board
  */
 public class TestGameBoard extends Board {
   /**
@@ -43,24 +49,44 @@ public class TestGameBoard extends Board {
     }
   }
 
+  /**
+   * Temporary method to add snake tiles to the board.
+   */
   public void addSnakeTiles() {
     tiles.put(14, new SnakeTile(14, 3, 3, 5));
     tiles.put(17, new SnakeTile(17, 6, 3, 2));
     tiles.put(31, new SnakeTile(31, 8, 4, 10));
   }
 
+  /**
+   * Temporary method to add ladder tiles to the board.
+   */
   public void addLadderTiles() {
     tiles.put(4, new LadderTile(4, 2, 3, 15));
     tiles.put(9, new LadderTile(9, 5, 2, 22));
     tiles.put(20, new LadderTile(20, 10, 3, 30));
   }
 
-  public Tile getTile(int tileNumber) {
+  /**
+   * Returns the tile object with the specified tile number.
+   *
+   * @param tileNumber the number of the tile.
+   * @return the tile object with the specified tile number.
+   * @throws IllegalArgumentException if the tile number does not exist,
+   *        (is not on the board).
+   */
+  public Tile getTile(final int tileNumber) throws IllegalArgumentException {
     if (!tiles.containsKey(tileNumber)) {
       throw new IllegalArgumentException("Tile number does not exist.");
     }
     return tiles.get(tileNumber);
   }
+
+  /**
+   * Returns the size of the board.
+   *
+   * @return the size of the board.
+   */
   public int getBoardSize() {
     return width * height;
   }
