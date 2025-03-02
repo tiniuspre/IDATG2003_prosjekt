44d5392ff607214d08c9a12e4610c9aa522d7323
xyfuss <jonasaatomren@gmail.com>
2025-03-02 14:57:54 +0100

    Refactored according to checkstyle and added documentation.

--- src/main/java/testgame/TestGame.java
+++ src/main/java/testgame/TestGame.java
@@ -1,57 +1,108 @@
 package testgame;
 
 import gameengine.dice.Dice;
-import testgame.engine.Player;
-import testgame.engine.TestGameBoard;
-import testgame.engine.tiles.SnakeTile;
-import testgame.engine.tiles.LadderTile;
-
 import java.util.ArrayList;
 import java.util.List;
+import testgame.engine.Player;
+import testgame.engine.TestGameBoard;
+import testgame.engine.tiles.LadderTile;
+import testgame.engine.tiles.SnakeTile;
 
+/**
+ * The {@code TestGame} class represents the test snaked and ladders game.
+ *
+ * <p>The game is played on a board with snakes and ladders.
+ * The players take turns to roll dice
+ * and move their pieces on the board. If a player lands on a snake tile,
+ * the player moves back to the tail of the snake.
+ * If a player lands on a ladder tile, the player climbs the ladder to the top.
+ * The game is won by the player who reaches the end of the board first.
+ *
+ * @author jonastomren
+ * @version 13.02.2025
+ * @since 13.02.2025
+ * @see TestGameBoard
+ * @see Player
+ * @see Dice
+ */
 public class TestGame {
-
-  private final TestGameBoard board = new TestGameBoard(10,9);
+  /**
+   * The game board with width 10 and height 9.
+   */
+  private final TestGameBoard board = new TestGameBoard(10, 9);
+  /**
+   * The list of players in the game.
+   */
   private final List<Player> players = new ArrayList<>();
+  /**
+   * The dice used in the game.
+   */
   private final Dice dice = new Dice(2);
 
+  /**
+   * Sets up the game board with snakes and ladders.
+   */
   public void setBoard() {
     board.createBoard();
     board.addSnakeTiles();
     board.addLadderTiles();
   }
 
-  public void addPlayer(String name) {
+  /**
+   * Adds a player to the game.
+   *
+   * @param name the name of the player.
+   */
+  public void addPlayer(final String name) {
     players.add(new Player(name));
   }
 
+  /**
+   * Plays one round of the game.
+   */
   public void playOneRound() {
     for (Player player : players) {
       int roll = dice.rollDice();
       if (player.getPosition() + roll >= board.getBoardSize()) {
         player.setPosition(board.getBoardSize());
-        System.out.println(player.getName() + " has reached the end of the board.");
+        System.out.println(player.getName()
+            + " has reached the end of the board.");
       } else {
         player.move(roll);
       }
-      if (board.getTile(player.getPosition()).getClass().equals(SnakeTile.class)) {
+      if (board.getTile(player.getPosition())
+          .getClass().equals(SnakeTile.class)) {
         SnakeTile snakeTile = (SnakeTile) board.getTile(player.getPosition());
         player.moveBack(snakeTile.getAction().landAction(player.getPosition()));
-        System.out.println(player.getName() + " landed on a snake and moved back to position " + player.getPosition());
-      } else if (board.getTile(player.getPosition()).getClass().equals(LadderTile.class)) {
-        LadderTile ladderTile = (LadderTile) board.getTile(player.getPosition());
+        System.out.println(player.getName()
+            + " landed on a snake and moved back to position "
+            + player.getPosition());
+      } else if (board.getTile(player.getPosition())
+          .getClass().equals(LadderTile.class)) {
+        LadderTile ladderTile = (LadderTile)
+            board.getTile(player.getPosition());
         player.move(ladderTile.getAction().landAction(player.getPosition()));
-        System.out.println(player.getName() + " climbed a ladder to position " + player.getPosition());
+        System.out.println(player.getName()
+            + " climbed a ladder to position " + player.getPosition());
       }
     }
   }
 
+  /**
+   * Prints the status of the players in the game with their current positions.
+   */
   public void printStatus() {
     for (Player player : players) {
-      System.out.println(player.getName() + " is at position " + player.getPosition());
+      System.out.println(player.getName()
+          + " is at position " + player.getPosition());
     }
   }
 
+  /**
+   * Returns the winner of the game.
+   *
+   * @return the winner of the game.
+   */
   public Player getWinner() {
     Player winner = players.getFirst();
     for (Player player : players) {
@@ -62,6 +113,11 @@ public class TestGame {
     return winner;
   }
 
+  /**
+   * Returns true if the game is finished.
+   *
+   * @return true if the game is finished.
+   */
   public boolean isFinished() {
     for (Player player : players) {
       if (player.getPosition() >= board.getBoardSize()) {
--- src/main/java/testgame/TestGameApp.java
+++ src/main/java/testgame/TestGameApp.java
@@ -1,19 +1,43 @@
 package testgame;
 
-public class TestGameApp {
-  static TestGame testGame = new TestGame();
+/**
+ * The {@code TestGameApp} class is the temporary main class
+ * for the test snaked and ladders game.
+ *
+ * @author jonastomren
+ * @since 13.02.2025
+ * @version 13.02.2025
+ * @see TestGame
+ */
+public final class TestGameApp {
+  /**
+   * Initializes a new {@code TestGame} snaked and ladders object.
+   */
+  private static final TestGame TEST_GAME = new TestGame();
 
-  public static void main(String[] args) {
-    testGame.addPlayer("Alice");
-    testGame.addPlayer("Bob");
-    testGame.setBoard();
-    testGame.printStatus();
-    while (!testGame.isFinished()) {
-      testGame.playOneRound();
-      if (!testGame.isFinished()) {
-        testGame.printStatus();
+  /**
+   * Private constructor to hide the implicit public one.
+   */
+  private TestGameApp() {
+    // Private constructor to hide the implicit public one.
+  }
+
+  /**
+   * The main method for the test snakes and ladders game.
+   *
+   * @param args the command-line arguments.
+   */
+  public static void main(final String[] args) {
+    TEST_GAME.addPlayer("Alice");
+    TEST_GAME.addPlayer("Bob");
+    TEST_GAME.setBoard();
+    TEST_GAME.printStatus();
+    while (!TEST_GAME.isFinished()) {
+      TEST_GAME.playOneRound();
+      if (!TEST_GAME.isFinished()) {
+        TEST_GAME.printStatus();
       }
     }
-    System.out.println(testGame.getWinner().getName() + " wins!");
+    System.out.println(TEST_GAME.getWinner().getName() + " wins!");
   }
 }
