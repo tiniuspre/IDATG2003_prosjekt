a3723c15f4fa81a82f4ee9ae0d9ef33966d31bbd
xyfuss <jonasaatomren@gmail.com>
2025-03-02 14:36:26 +0100

    Refactored according to checkstyle and added documentation.

--- src/main/java/testgame/engine/actions/LadderAction.java
+++ src/main/java/testgame/engine/actions/LadderAction.java
@@ -2,31 +2,75 @@ package testgame.engine.actions;
 
 import gameengine.board.TileAction;
 
+/**
+ * The {@code LadderAction} class represents the action,
+ * of a ladder in the test snakes and ladders game.
+ *
+ * @author jonastomren
+ * @version 13.02.2025
+ * @since 13.02.2025
+ * @see TileAction
+ */
 public class LadderAction implements TileAction {
+  /**
+   * The bottom of the ladder.
+   */
   private int ladderBottom;
+  /**
+   * The top of the ladder.
+   */
   private int ladderTop;
 
-  public LadderAction(int ladderBottom, int ladderTop) {
-    setLadderBottom(ladderBottom);
-    setLadderTop(ladderTop);
+  /**
+   * Constructor for the LadderAction class.
+   *
+   * @param inputLadderBottom the bottom of the ladder.
+   * @param inputLadderTop the top of the ladder.
+   */
+  public LadderAction(final int inputLadderBottom, final int inputLadderTop) {
+    setLadderBottom(inputLadderBottom);
+    setLadderTop(inputLadderTop);
   }
 
-  public void setLadderBottom(int ladderBottom) {
-    if (ladderBottom < 0) {
-      throw new IllegalArgumentException("Ladder bottom must be greater than 0.");
+  /**
+   * Sets the bottom of the ladder.
+   *
+   * @param inputLadderBottom the bottom of the ladder.
+   * @throws IllegalArgumentException if the ladder bottom is less than 0.
+   */
+  public void setLadderBottom(final int inputLadderBottom)
+      throws IllegalArgumentException {
+    if (inputLadderBottom < 0) {
+      throw new IllegalArgumentException("Ladder "
+          + "bottom must be greater than 0.");
     }
-    this.ladderBottom = ladderBottom;
+    this.ladderBottom = inputLadderBottom;
   }
 
-  public void setLadderTop(int ladderTop) {
-    if (ladderTop < 0 || ladderTop <= ladderBottom) {
-      throw new IllegalArgumentException("Ladder top must be greater than 0 and greater than ladder bottom.");
+  /**
+   * Sets the top of the ladder.
+   *
+   * @param inputLadderTop the top of the ladder.
+   * @throws IllegalArgumentException if the ladder top is less than 0,
+   *        or less than ladder bottom.
+   */
+  public void setLadderTop(final int inputLadderTop)
+      throws IllegalArgumentException {
+    if (inputLadderTop < 0 || inputLadderTop <= ladderBottom) {
+      throw new IllegalArgumentException("Ladder top must "
+          + "be greater than 0 and greater than ladder bottom.");
     }
-    this.ladderTop = ladderTop;
+    this.ladderTop = inputLadderTop;
   }
 
+  /**
+   * The action that should be performed when a player lands on the tile.
+   *
+   * @param position the position of the player.
+   * @return the position of the player after the action.
+   */
   @Override
-  public int landAction(int position) {
+  public int landAction(final int position) {
     return ladderTop;
   }
 }
