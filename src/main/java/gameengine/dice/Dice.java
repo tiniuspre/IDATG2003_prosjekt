93d88a295ee6ede1cbbe402238841c06db5b5f5e
xyfuss <jonasaatomren@gmail.com>
2025-03-06 15:03:43 +0100

    Added documentation for Dice class.

--- src/main/java/gameengine/dice/Dice.java
+++ src/main/java/gameengine/dice/Dice.java
@@ -10,30 +10,47 @@ import java.util.List;
  * @version 13.02.2025
  * @since 13.02.2025
  */
+
 public class Dice {
   /**
    * The number of dice.
    */
-  private int numberOfDice;
+  private final int numberOfDice;
   /**
    * The list of dice.
    */
   private final List<Die> dice = new ArrayList<>();
 
-  public Dice(final int numberOfDice) {
-    if (numberOfDice < 1) {
+  /**
+   * Constructor for the dice class, makes the number of Die input.
+   *
+   * @param inputNumOfDice the number of die to be made.
+   * @throws IllegalArgumentException if the number of die is less than 1.
+   */
+  public Dice(final int inputNumOfDice) throws IllegalArgumentException {
+    if (inputNumOfDice < 1) {
       throw new IllegalArgumentException("Number of dice must be at least 1.");
     }
-    for (int i = 0; i < numberOfDice; i++) {
+    this.numberOfDice = inputNumOfDice;
+    for (int i = 0; i < inputNumOfDice; i++) {
       dice.add(new Die(i));
     }
   }
 
+  /**
+   * Rolls all the dice.
+   *
+   * @return the sum of all the dice rolled.
+   */
   public int rollDice() {
     int sum = 0;
     for (Die die : dice) {
       sum += die.roll();
     }
     return sum;
+  }
+
+  public int getNumberOfDice() {
+    return numberOfDice;
   }
 }
