c8900b1f9431b43fd1b8fdcde87b762d9d6e381f
xyfuss <jonasaatomren@gmail.com>
2025-03-06 14:47:37 +0100

    Added mutator and accessor methods for Tile class fields.

--- src/main/java/gameengine/board/Tile.java
+++ src/main/java/gameengine/board/Tile.java
@@ -15,7 +15,7 @@ public abstract class Tile {
   /**
    * The number of the tile.
    */
-  private final int number;
+  private int number;
   /**
    * The action of the tile.
    */
@@ -23,11 +23,11 @@ public abstract class Tile {
   /**
    * The x position of the tile.
    */
-  private final int posX;
+  private int posX;
   /**
    * The y position of the tile.
    */
-  private final int posY;
+  private int posY;
 
   /**
    * Constructs a new {@code Tile} object
@@ -38,14 +38,11 @@ public abstract class Tile {
    * @param positionY the y position of the tile.
    */
   public Tile(final int num, final int positionX, final int positionY) {
-    this.number = num;
-    this.posX = positionX;
-    this.posY = positionY;
+    setNumber(num);
+    setPosX(positionX);
+    setPosY(positionY);
   }
 
-  public final int getNumber() {
-    return number;
-  }
 
   public final TileAction getAction() {
     return action;
@@ -55,11 +52,36 @@ public abstract class Tile {
     this.action = tileAction;
   }
 
-  public final int getPosX() {
+  public int getNumber() {
+    return number;
+  }
+
+  public int getPosX() {
     return posX;
   }
 
-  public final int getPosY() {
+  public int getPosY() {
     return posY;
+  }
+
+  public void setPosX(final int inputPosX) {
+    if (inputPosX < 0 ) {
+      throw new IllegalArgumentException("Invalid x position of tile.");
+    }
+    this.posX = inputPosX;
+  }
+
+  public void setPosY(final int inputPosY) {
+    if (inputPosY < 0 ) {
+      throw new IllegalArgumentException("Invalid y position of tile.");
+    }
+    this.posY = inputPosY;
+  }
+
+  public void setNumber(final int inputNumber) {
+    if (inputNumber < 0) {
+      throw new IllegalArgumentException("Invalid number of tile.");
+    }
+    this.number = inputNumber;
   }
 }
