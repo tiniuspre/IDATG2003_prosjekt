5c46e4ad6b1e6f36b3dbc9da5ad0063ab9b63d66
xyfuss <jonasaatomren@gmail.com>
2025-03-06 15:19:41 +0100

    Refactored test class for Die class.

--- src/test/java/gameengine/dice/DieTest.java
+++ src/test/java/gameengine/dice/DieTest.java
@@ -3,6 +3,8 @@ package gameengine.dice;
 import junit.framework.TestCase;
 import org.junit.Assert;
 
+import static org.junit.Assert.assertThrows;
+
 /**
  * The {@code DieTest} class tests the behavior of the {@code Die} class.
  */
@@ -12,5 +14,14 @@ public class DieTest extends TestCase {
     Die test = new Die(1);
     int result = test.roll();
     Assert.assertTrue(result >= 1 && result <= 6);
+
   }
+
+  public void testSetDieNumber() {
+    Die test = new Die(1);
+    test.setDieNumber(2);
+    assertEquals(2,test.getDieNumber());
+    assertThrows(IllegalArgumentException.class, () -> test.setDieNumber(-1));
+  }
+
 }
