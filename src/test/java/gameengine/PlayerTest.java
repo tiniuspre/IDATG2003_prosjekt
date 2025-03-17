c4b4dbe73d76ec2eb0652c00e69efec5bb92bf87
xyfuss <jonasaatomren@gmail.com>
2025-03-17 13:07:48 +0100

    Refactored test case according to JUnit 5.

--- src/test/java/gameengine/PlayerTest.java
+++ src/test/java/gameengine/PlayerTest.java
@@ -1,30 +1,32 @@
-package snakesandladders.engine;
+package gameengine;
 
-import gameengine.Player;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import snakesandladders.engine.SnakesAndLaddersPlayer;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class PlayerTest extends TestCase {
+class PlayerTest {
 
   static Player testPlayer = new SnakesAndLaddersPlayer("test");
 
+  @Test
   public void testTestSetName() {
     assertThrows(IllegalArgumentException.class, () -> new SnakesAndLaddersPlayer(""));
   }
 
+  @Test
   public void testSetPosition() {
     testPlayer.setPosition(2);
     assertEquals(2,testPlayer.getPosition());
     assertThrows(IllegalArgumentException.class, () -> testPlayer.setPosition(-1));
   }
-
+  @Test
   public void testMove() {
     testPlayer.move(3);
     assertEquals(3,testPlayer.getPosition());
     assertThrows(IllegalArgumentException.class,() -> testPlayer.move(-1));
   }
-
+  @Test
   public void testMoveBack() {
     testPlayer.setPosition(5);
     testPlayer.moveBack(4);
