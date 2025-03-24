137fc68b09066772366678ad9937536b6ea320b9
xyfuss <jonasaatomren@gmail.com>
2025-03-24 21:01:20 +0100

    Renamed CsvHandler

---
+++ src/main/java/filehandler/CsvHandler.java
@@ -0,0 +1,12 @@
+package filehandler;
+
+/**
+ * The {@code CsvHandler} class handles all writing, and reading to and from the csv files.
+ *
+ * @author jonastomren
+ * @version 15.03.2025
+ * @since 15.03.2025
+ */
+public class CsvHandler {
+
+}
