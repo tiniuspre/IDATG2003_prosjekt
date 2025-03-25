98e324f3a0da99210896611b98a518a1783ec50c
xyfuss <jonasaatomren@gmail.com>
2025-03-25 12:50:52 +0100

    add CsvHandlerException

---
+++ src/main/java/filehandler/CsvHandlerException.java
@@ -0,0 +1,19 @@
+package filehandler;
+
+/**
+ * Exception thrown when an error occurs in the CsvHandler class.
+ *
+ * @since 25.03.2025
+ * @version 25.03.2025
+ * @author jonastomren
+ */
+public class CsvHandlerException extends RuntimeException {
+  /**
+   * Constructor for the CsvHandlerException class.
+   *
+   * @param message The error message.
+   */
+  public CsvHandlerException(final String message) {
+    super(message);
+  }
+}
