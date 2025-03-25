ef1ac6ed294331eddc9683e189b2e35ceb832552
xyfuss <jonasaatomren@gmail.com>
2025-03-25 22:53:33 +0100

    add docs and file paths

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -6,14 +6,30 @@ package constants;
  *
  * @since 25.03.2025
  * @version 25.03.2025
- * @author tinius
+ * @author tinius, jonastomren
  */
 public final class Constants {
 
   // File paths
+  /**
+   * Path to the Snakes and Ladders board JSON file.
+   */
   public static final String SNL_BOARD_FILE_PATH = "json-files/snakes_board.json";
-  public static final String SNL_PLAYER_FILE_PATH = "json-files/snakes_players.csv";
-  public static final String SNL_PLAYER_FILE_PATH_TEST = "src/test/resources/json-files/snakes_players_test.csv";
+
+  /**
+   * Path to the Snakes and Ladders players CSV file.
+   */
+  public static final String SNL_PLAYER_FILE_PATH = "csv-files/snakes_players.csv";
+
+  /**
+   * Path to the test Snakes and Ladders players CSV file.
+   */
+  public static final String SNL_PLAYER_FILE_PATH_TEST = "src/test/resources/csv-files/snakes_players_test.csv";
+
+  /**
+   * Private constructor to prevent instantiation.
+   * Throws IllegalStateException if called.
+   */
   Constants() {
     throw new IllegalStateException("Should not be instantiated");
   }
