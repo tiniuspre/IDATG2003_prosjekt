97a3dc59752562d0b2a871dd621988501a9f2d4b
xyfuss <jonasaatomren@gmail.com>
2025-03-31 14:35:32 +0200

    add package-info and fix checkstyle

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -14,23 +14,26 @@ public final class Constants {
   /**
    * Path to the Snakes and Ladders board JSON file.
    */
-  public static final String SNL_BOARD_FILE_PATH = "json-files/snakes_board.json";
+  public static final String SNL_BOARD_FILE_PATH =
+      "json-files/snakes_board.json";
 
   /**
    * Path to the Snakes and Ladders players CSV file.
    */
-  public static final String SNL_PLAYER_FILE_PATH = "csv-files/snakes_players.csv";
+  public static final String SNL_PLAYER_FILE_PATH =
+      "csv-files/snakes_players.csv";
 
   /**
    * Path to the test Snakes and Ladders players CSV file.
    */
-  public static final String SNL_PLAYER_FILE_PATH_TEST = "src/test/resources/csv-files/snakes_players_test.csv";
+  public static final String SNL_PLAYER_FILE_PATH_TEST =
+      "src/test/resources/csv-files/snakes_players_test.csv";
 
   /**
    * Private constructor to prevent instantiation.
    * Throws IllegalStateException if called.
    */
-  Constants() {
+  private Constants() {
     throw new IllegalStateException("Should not be instantiated");
   }
 }
