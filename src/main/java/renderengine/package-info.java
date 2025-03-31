6a37ce42dd8d5f21b1942f3cc138430a06abc3cd
Tinius Presterud <github@tinius.dev>
2025-03-31 14:29:53 +0200

    Add javadoc to package-info

--- src/main/java/renderengine/package-info.java
+++ src/main/java/renderengine/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * This package contains the classes that are responsible for rendering the game.
+ */
 package renderengine;
--- src/main/java/snakesandladders/ui/package-info.java
+++ src/main/java/snakesandladders/ui/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * User interface classes.
+ */
 package snakesandladders.ui;
