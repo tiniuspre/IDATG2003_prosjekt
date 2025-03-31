595acb8e8ee689e55e34b7a0ca35e13cb07eccab
Tinius Presterud <github@tinius.dev>
2025-03-31 14:40:29 +0200

    Update src/main/java/renderengine/AbstractTileUI.java
    Co-authored-by: Jonas Tomren <79199628+xyfuss@users.noreply.github.com>

--- src/main/java/renderengine/AbstractTileUI.java
+++ src/main/java/renderengine/AbstractTileUI.java
@@ -6,9 +6,9 @@ import javafx.scene.Node;
  * Abstract base class for rendering a single tile on the board.
  * Subclasses could define tile color, shape, or images.
  *
- * \@author tiniuspre
- * \@version 25.03.2025
- * \@since 25.03.2025
+ * @author tiniuspre
+ * @version 25.03.2025
+ * @since 25.03.2025
  */
 public abstract class AbstractTileUI {
 
