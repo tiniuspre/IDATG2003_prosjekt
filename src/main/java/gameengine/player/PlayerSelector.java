b49176f81c7f4c476e84ebd2b1731ef6889591eb
xyfuss <jonasaatomren@gmail.com>
2025-04-14 15:17:05 +0200

    add docs

--- src/main/java/gameengine/player/PlayerSelector.java
+++ src/main/java/gameengine/player/PlayerSelector.java
@@ -1,20 +1,51 @@
 package gameengine.player;
 
+import java.util.List;
 import snakesandladders.engine.SnakesAndLaddersPlayer;
 
-import java.util.List;
-
+/**
+ * The {@code PlayerSelector} class is responsible for
+ * selecting players in the game.
+ * It provides functionality to select a random player
+ * from a list of players, excluding the current player.
+ *
+ * @author jonastomren
+ * @version 14.04.2025
+ * @since 14.04.2025
+ * @see SnakesAndLaddersPlayer
+ */
 public class PlayerSelector {
-
+  /**
+   * The list of all players in the game.
+   */
   private final List<SnakesAndLaddersPlayer> allPlayers;
-
+  /**
+   * The current player in the game.
+   */
   private final Player current;
 
-  public PlayerSelector(final List<SnakesAndLaddersPlayer> players, final Player currentPlayer) {
+  /**
+   * Constructs a {@code PlayerSelector} with the specified list
+   * of players and the current player.
+   *
+   * @param players The list of all players in the game.
+   * @param currentPlayer The current player in the game.
+   */
+  public PlayerSelector(final List<SnakesAndLaddersPlayer> players,
+                        final Player currentPlayer) {
     this.allPlayers = players;
     this.current = currentPlayer;
   }
 
+  /**
+   * Selects a random player from the list of players,
+   * excluding the current player.
+   *
+   * @return A randomly selected {@code Player}
+   *        from the list of available players.
+   * @throws IllegalArgumentException If no players are
+   *        available to select from.
+   */
   public Player selectRandomPlayer() {
     // Filter out the current player from the list
     List<SnakesAndLaddersPlayer> availablePlayers = allPlayers.stream()
@@ -22,9 +53,11 @@ public class PlayerSelector {
         .toList();
     // If no players are available, throw an exception
     if (availablePlayers.isEmpty()) {
-      throw new IllegalArgumentException("No available players to select from.");
+      throw new IllegalArgumentException(
+          "No available players to select from.");
     }
     // Select a random player from the available players
-    return availablePlayers.get((int) (Math.random() * availablePlayers.size()));
+    return availablePlayers.get((int)
+        (Math.random() * availablePlayers.size()));
   }
 }
