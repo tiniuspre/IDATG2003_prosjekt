14ad44824dfe1b12bfcc137bd8791a38c66eaee5
xyfuss <jonasaatomren@gmail.com>
2025-04-14 13:35:46 +0200

    added name field and removed tiles

--- src/main/java/gameengine/board/Board.java
+++ src/main/java/gameengine/board/Board.java
@@ -14,10 +14,6 @@ import java.util.Map;
 
 public abstract class Board {
   /**
-   * The tiles of the board.
-   */
-  private final Map<Integer, Tile> tiles = new HashMap<>();
-  /**
    * The width of the board.
    */
   private int width;
@@ -25,6 +21,10 @@ public abstract class Board {
    * The height of the board.
    */
   private int height;
+  /**
+   * The name of the board.
+   */
+  private String name;
   /**
    * Constructs a new {@code Board} object
    * with the specified inputWidth and inputHeight.
@@ -35,6 +35,13 @@ public abstract class Board {
   public Board(final int inputWidth, final int inputHeight) {
     setWidth(inputWidth);
     setHeight(inputHeight);
+  }
+
+  /**
+   * Default constructor for the Board class.
+   */
+  public Board() {
+    // Default constructor
   }
   /**
    * Creates the board.
@@ -66,14 +73,6 @@ public abstract class Board {
     }
     this.height = inputHeight;
   }
-  /**
-   * Returns the tiles of the board.
-   *
-   * @return the tiles of the board.
-   */
-  public final Map<Integer, Tile> getTiles() {
-    return tiles;
-  }
 
   /**
    * Accessor method for the width of the board.
@@ -91,5 +90,27 @@ public abstract class Board {
    */
   public final int getHeight() {
     return height;
+  }
+
+  /**
+   * Accessor method for the name of the board.
+   *
+   * @return the name of the board.
+   */
+  public final String getName() {
+    return name;
+  }
+
+  /**
+   * Sets the name of the board.
+   *
+   * @param inputName the name of the board.
+   * @throws IllegalArgumentException if the name is null or empty.
+   */
+  public void setName(final String inputName) throws IllegalArgumentException {
+    if (inputName == null || inputName.isBlank()) {
+      throw new IllegalArgumentException("Invalid name of board.");
+    }
+    this.name = inputName;
   }
 }
