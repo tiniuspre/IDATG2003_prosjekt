3dd7075757afe4e76863580360a8007ce06d7bd2
xyfuss <jonasaatomren@gmail.com>
2025-04-14 15:18:35 +0200

    add docs

--- src/main/java/snakesandladders/engine/actions/Switch.java
+++ src/main/java/snakesandladders/engine/actions/Switch.java
@@ -3,26 +3,56 @@ package snakesandladders.engine.actions;
 import gameengine.player.Player;
 import gameengine.player.PlayerSelector;
 
+/**
+ * The {@code Switch} class represents a switch action in the
+ * snakes and ladders game.
+ *
+ * @author jonastomren
+ * @version 14.04.2025
+ * @since 14.04.2025
+ * @see SpecialAction
+ */
 public class Switch implements SpecialAction {
-
+  /**
+   * Utility for selecting players.
+   */
   private final PlayerSelector playerSelector;
 
+  /**
+   * Constructs a {@code Switch} action with the specified player selector.
+   *
+   * @param playerContext The {@code PlayerSelector}
+   *                     used to select a random target player.
+   */
   public Switch(final PlayerSelector playerContext) {
     this.playerSelector = playerContext;
   }
 
+  /**
+   * Applies the switch action by swapping the positions of the current player
+   * and a randomly selected target player.
+   *
+   * @param currentPlayer The player who initiates the switch action.
+   */
   @Override
   public void apply(final Player currentPlayer) {
+    // Select a random target player
     Player targetPlayer = playerSelector.selectRandomPlayer();
 
+    // Get the positions of the current player and the target player
     int currentPlayerPosition = currentPlayer.getPosition();
     int targetPlayerPosition = targetPlayer.getPosition();
 
+    // Swap the positions of the current player and the target player
     currentPlayer.setPosition(targetPlayerPosition);
     targetPlayer.setPosition(currentPlayerPosition);
 
-    System.out.println(currentPlayer.getName() + " switched positions with " + targetPlayer.getName() + "! " +
-        currentPlayer.getName() + " is now at " + targetPlayerPosition + " and " +
-        targetPlayer.getName() + " is now at " + currentPlayerPosition);
+    // Print the result of the switch action
+    System.out.println(currentPlayer.getName()
+        + " switched positions with " + targetPlayer.getName() + "! "
+        + currentPlayer.getName() + " is now at "
+        + targetPlayerPosition + " and "
+        + targetPlayer.getName() + " is now at "
+        + currentPlayerPosition);
   }
 }
