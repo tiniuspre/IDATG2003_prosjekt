4265866e84fdf391809f97fada998078f7f9096e
xyfuss <jonasaatomren@gmail.com>
2025-04-15 15:52:57 +0200

    add docs

--- src/main/java/snakesandladders/engine/SnakesAndLaddersPiece.java
+++ src/main/java/snakesandladders/engine/SnakesAndLaddersPiece.java
@@ -1,5 +1,14 @@
 package snakesandladders.engine;
 
+/**
+ * The {@code SnakesAndLaddersPiece} enum represents the
+ * different pieces that can be used in the Snakes and Ladders game.
+ *
+ * @author jonastomren
+ * @version 15.04.2025
+ * @since 15.04.2025
+ * @see SnakesAndLaddersPlayer
+ */
 public enum SnakesAndLaddersPiece {
   HAT, CAR, DOG, BOOT, CAT, WHEELBARROW
 }
