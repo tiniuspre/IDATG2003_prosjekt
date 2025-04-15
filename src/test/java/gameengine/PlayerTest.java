45a21cb5553b85a04a91dde43ae023cee0e282f3
xyfuss <jonasaatomren@gmail.com>
2025-04-15 14:39:26 +0200

    add DisplayName

--- src/test/java/gameengine/PlayerTest.java
+++ src/test/java/gameengine/PlayerTest.java
@@ -1,6 +1,7 @@
 package gameengine;
 
 import gameengine.player.Player;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import snakesandladders.engine.SnakesAndLaddersPlayer;
 
@@ -11,11 +12,13 @@ class PlayerTest {
   static Player testPlayer = new SnakesAndLaddersPlayer("test","hat");
 
   @Test
+  @DisplayName("Test for setting player name")
   public void testTestSetName() {
     assertThrows(IllegalArgumentException.class, () -> new SnakesAndLaddersPlayer("",""));
   }
 
   @Test
+  @DisplayName("Test for setting player position")
   public void testSetPosition() {
     testPlayer.setPosition(2);
     assertEquals(2,testPlayer.getPosition());
