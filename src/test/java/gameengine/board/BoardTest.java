b96a0ce0d5b7b5c66351f482bc86fffa76875753
xyfuss <jonasaatomren@gmail.com>
2025-04-15 14:38:59 +0200

    add DisplayName

--- src/test/java/gameengine/board/BoardTest.java
+++ src/test/java/gameengine/board/BoardTest.java
@@ -1,5 +1,6 @@
 package gameengine.board;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import snakesandladders.engine.SnakesAndLaddersBoard;
 
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class BoardTest {
 
   @Test
+  @DisplayName("Test for setting width")
   public void testSetWidth() {
     SnakesAndLaddersBoard testBoard = new SnakesAndLaddersBoard(2,3);
     assertThrows(IllegalArgumentException.class, () -> new SnakesAndLaddersBoard(-1,3));
@@ -15,6 +17,7 @@ class BoardTest {
   }
 
   @Test
+  @DisplayName("Test for setting height")
   public void testSetHeight() {
     SnakesAndLaddersBoard testBoard = new SnakesAndLaddersBoard(2,3);
     assertThrows(IllegalArgumentException.class, () -> new SnakesAndLaddersBoard(3,-1));
