62eca2608ae7b884c5348c796025ecce78ef29d8
xyfuss <jonasaatomren@gmail.com>
2025-04-15 15:44:04 +0200

    removed Piece class

--- src/test/java/snakesandladders/engine/SnakesAndLaddersPlayerTest.java
+++ src/test/java/snakesandladders/engine/SnakesAndLaddersPlayerTest.java
@@ -11,7 +11,6 @@ class SnakesAndLaddersPlayerTest {
   @Test
   void setPiece() {
     SnakesAndLaddersPlayer player = new SnakesAndLaddersPlayer("Jonas", "hat");
-    assertEquals(SnakesAndLaddersPiece.HAT.getValue(), player.getPiece());
     assertThrows(IllegalArgumentException.class, () -> new SnakesAndLaddersPlayer("Jonas", ""));
   }
 
