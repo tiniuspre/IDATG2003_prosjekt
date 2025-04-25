94530feb8e1e912d7b2510f7918b3e5d048a7f5f
xyfuss <jonasaatomren@gmail.com>
2025-04-25 16:29:13 +0200

    add snl tile

--- src/main/java/snakesandladders/engine/board/tile/SnakesAndLaddersTile.java
+++ src/main/java/snakesandladders/engine/board/tile/SnakesAndLaddersTile.java
@@ -1,5 +1,6 @@
 package snakesandladders.engine.board.tile;
 
+import constants.Constants;
 import gameengine.board.tile.Tile;
 
 public class SnakesAndLaddersTile extends Tile {
@@ -7,6 +8,8 @@ public class SnakesAndLaddersTile extends Tile {
    * The type of the tile.
    */
   private String type;
+
+  private int next;
 
   /**
    * Constructor for the SnakesAndLaddersTile class.
@@ -36,5 +39,20 @@ public class SnakesAndLaddersTile extends Tile {
    */
   public String getType() {
     return type;
+  }
+
+  public int getNext() {
+    return next;
+  }
+
+  public void setNext(final int next) {
+    if (next < 0) {
+      throw new IllegalArgumentException("Invalid tile position.");
+    } else if ( next > getPosition() && type.equals(Constants.SNAKE) ) {
+      throw new IllegalArgumentException("Invalid snake position.");
+    } else if ( next < getPosition() && type.equals(Constants.LADDER) ) {
+      throw new IllegalArgumentException("Invalid ladder position.");
+    }
+    this.next = next;
   }
 }
