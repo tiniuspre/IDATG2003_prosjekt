4df3c1b9904ea4443b695352b66d1329c42bf613
xyfuss <jonasaatomren@gmail.com>
2025-04-25 19:57:55 +0200

    refactor loadBoard method to handle exceptions with SnLBoardException

--- src/main/java/snakesandladders/engine/board/SnLLoader.java
+++ src/main/java/snakesandladders/engine/board/SnLLoader.java
@@ -28,21 +28,26 @@ public final class SnLLoader {
    * @param boardName The name of the board to load.
    * @return The SnakesAndLaddersBoard object
    *        corresponding to the specified board name.
-   * @throws IOException If an I/O error occurs
-   *        while reading the JSON file.
    * @throws IllegalArgumentException If the board is not found
    *        or the JSON file is invalid.
    * @throws JsonHandlerException If there is an error
    *        while processing the JSON file.
    */
-  public static SnLBoard loadBoard(final String boardName)
-      throws IOException {
-    // Creates a handler for the JSON file containing the boards
-    JsonHandler jsonHandler = new JsonHandler("boards/snl/"
-        + boardName + ".json");
-    // Reads the list of all boards from the JSON file
-    List<SnLBoardConfig> boardConfigs = jsonHandler
-        .readFromFile(SnLBoardConfig.class);
+  public static SnLBoard loadBoard(final String boardName) {
+    // Declare the JsonHandler and boardConfigs variables
+    JsonHandler jsonHandler;
+    List<SnLBoardConfig> boardConfigs;
+    try {
+      // Creates a handler for the JSON file containing the boards
+      jsonHandler = new JsonHandler("boards/snl/"
+          + boardName + ".json");
+      // Reads the list of all boards from the JSON file
+      boardConfigs = jsonHandler
+          .readFromFile(SnLBoardConfig.class);
+    } catch (IOException e) {
+      throw new SnLBoardException("Failed to create JSON handler: "
+          + e.getMessage());
+    }
     // Checks if the boardConfigs list is null or empty
     if (boardConfigs == null) {
       throw new JsonHandlerException(
@@ -50,6 +55,7 @@ public final class SnLLoader {
     } else if (boardConfigs.isEmpty()) {
       throw new JsonHandlerException("No boards found in JSON file.");
     }
+    // Creates the board using the first board configuration
     try {
       return new SnLBoard(Constants.SNL_WIDTH,
           Constants.SNL_HEIGHT,
@@ -58,8 +64,8 @@ public final class SnLLoader {
       throw new SnLBoardException("Invalid board configuration: "
           + e.getMessage());
     } catch (JsonHandlerException e) {
-      throw new JsonHandlerException("Error processing JSON file: "
-          + e.getMessage());
+      throw new SnLBoardException(
+          "Error getting board when processing JSON file: " + e.getMessage());
     }
   }
 }
