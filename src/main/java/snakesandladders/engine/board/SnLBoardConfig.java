b722418967f363f5be81487ef698928b877a1880
xyfuss <jonasaatomren@gmail.com>
2025-04-25 16:40:59 +0200

    fix docs SnLBoardConfig

--- src/main/java/snakesandladders/engine/board/SnLBoardConfig.java
+++ src/main/java/snakesandladders/engine/board/SnLBoardConfig.java
@@ -1,6 +1,7 @@
 package snakesandladders.engine.board;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import snakesandladders.engine.board.tile.Jump;
 
 import java.util.ArrayList;
@@ -9,26 +10,46 @@ import java.util.stream.Stream;
 
 public final class SnLBoardConfig {
 
-
+  /**
+   * The list of jumps on the board.
+   */
   private final List<Jump> jumps = new ArrayList<>();
-
+  /**
+   * The list of snakes on the board.
+   */
   private final List<Jump> snakes = new ArrayList<>();
+  /**
+   * The list of ladders on the board.
+   */
   private final List<Jump> ladders = new ArrayList<>();
+  /**
+   * The list of switches on the board.
+   */
   private final List<Jump> switches = new ArrayList<>();
 
   /**
    * Constructor for the SnLBoardConfig class.
+   *
+   * @param inputJumps the list of jumps to configure.
    */
   @JsonCreator
   public SnLBoardConfig(final List<Jump> inputJumps) {
     setJumps(inputJumps);
   }
 
+  /**
+   * Default constructor for the SnLBoardConfig class.
+   * This constructor is used for JSON deserialization.
+   */
   public SnLBoardConfig() {
     // Default constructor
   }
 
-
+  /**
+   * Configures the jumps on the board based on the input list.
+   *
+   * @param inputJumps the list of jumps to configure.
+   */
   public void configureJumps(final List<Jump> inputJumps) {
     for (Jump jump : inputJumps) {
       if (jump.getFrom() > jump.getTo()) {
@@ -41,25 +62,42 @@ public final class SnLBoardConfig {
     }
   }
 
+  /**
+   * Sets the jumps on the board.
+   *
+   * @param inputJumps
+   */
+  @JsonSetter("jumps")
   public void setJumps(final List<Jump> inputJumps) {
     this.jumps.clear();
     this.jumps.addAll(inputJumps);
     configureJumps(inputJumps);
   }
 
+  /**
+   * Gets the list of snakes on the board.
+   *
+   * @return a stream of the list of snakes on the board.
+   */
   public Stream<Jump> getSnakes() {
     return snakes.stream();
   }
 
+  /**
+   * Gets the list of ladders on the board.
+   *
+   * @return a stream of the list of ladders on the board.
+   */
   public Stream<Jump> getLadders() {
     return ladders.stream();
   }
 
+  /**
+   * Gets the list of switches on the board.
+   *
+   * @return a stream of the list of switches on the board.
+   */
   public Stream<Jump> getSwitches() {
     return switches.stream();
-  }
-
-  public Stream<Jump> getJumps() {
-    return jumps.stream();
   }
 }
