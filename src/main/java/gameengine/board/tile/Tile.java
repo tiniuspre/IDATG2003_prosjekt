8a337319ce2b81444de0e72cef7f57292c92da40
xyfuss <jonasaatomren@gmail.com>
2025-04-25 16:47:36 +0200

    fix docs Tile

--- src/main/java/gameengine/board/tile/Tile.java
+++ src/main/java/gameengine/board/tile/Tile.java
@@ -1,13 +1,26 @@
 package gameengine.board.tile;
 
 public abstract class Tile {
-
+  /**
+   * The position of the tile on the board.
+   */
   private int position;
 
+  /**
+   * Constructor for the Tile class.
+   *
+   * @param inputPosition the position of the tile on the board.
+   */
   public Tile(final int inputPosition) {
     setPosition(inputPosition);
   }
 
+  /**
+   * Sets the position of the tile.
+   *
+   * @param inputPosition the position of the tile on the board.
+   * @throws IllegalArgumentException if the position is invalid.
+   */
   public void setPosition(final int inputPosition)
       throws IllegalArgumentException {
     if (inputPosition < 0) {
@@ -16,6 +29,11 @@ public abstract class Tile {
     this.position = inputPosition;
   }
 
+  /**
+   * Gets the position of the tile.
+   *
+   * @return the position of the tile on the board.
+   */
   public int getPosition() {
     return position;
   }
