407c0cd4912d672aefd17b4bfa9606b52c40bf12
xyfuss <jonasaatomren@gmail.com>
2025-04-25 15:13:54 +0200

    add SnLBoardConfig class for board configuration

---
+++ src/main/java/snakesandladders/engine/board/SnLBoardConfig.java
@@ -0,0 +1,10 @@
+package snakesandladders.engine.board;
+
+import snakesandladders.engine.board.tile.Jump;
+
+import java.util.List;
+
+public final class SnLBoardConfig {
+  private List<Jump> snakes;
+  private List<Jump> ladders;
+}
