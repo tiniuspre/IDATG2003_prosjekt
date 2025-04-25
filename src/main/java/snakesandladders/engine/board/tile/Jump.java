2b4e2cca476745aa612ffd62d1e32baee924ca53
xyfuss <jonasaatomren@gmail.com>
2025-04-25 16:36:29 +0200

    fix docs Jump

--- src/main/java/snakesandladders/engine/board/tile/Jump.java
+++ src/main/java/snakesandladders/engine/board/tile/Jump.java
@@ -4,8 +4,14 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonSetter;
 
 public class Jump {
-  public int from;
-  public int to;
+  /**
+   * The starting position of the jump.
+   */
+  private int from;
+  /**
+   * The ending position of the jump.
+   */
+  private int to;
 
   /**
    * Constructor for the Jump class.
@@ -18,29 +24,57 @@ public class Jump {
     setTo(inputTo);
   }
 
+  /**
+   * Default constructor for the Jump class.
+   * Only to be used for file handling.
+   */
   public Jump() {
     // Default constructor
   }
+
+  /**
+   * Gets the ending position of the jump.
+   *
+   * @return the ending position of the jump.
+   */
   @JsonGetter("to")
   public int getTo() {
     return to;
   }
+
+  /**
+   * Gets the starting position of the jump.
+   *
+   * @return the starting position of the jump.
+   */
   @JsonGetter("from")
   public int getFrom() {
     return from;
   }
-  @JsonSetter("from")
-  public void setFrom(final int from) {
-    if (from < 0) {
+
+  /**
+   * Sets the starting position of the jump.
+   *
+   * @param inputFrom the starting position of the jump.
+   */
+  @JsonSetter("inputFrom")
+  public void setFrom(final int inputFrom) {
+    if (inputFrom < 0) {
       throw new IllegalArgumentException("Invalid jump position.");
     }
-    this.from = from;
+    this.from = inputFrom;
   }
-  @JsonSetter("to")
-  public void setTo(final int to) {
-    if (to < 0) {
+
+  /**
+   * Sets the ending position of the jump.
+   *
+   * @param inputTo the ending position of the jump.
+   */
+  @JsonSetter("inputTo")
+  public void setTo(final int inputTo) {
+    if (inputTo < 0) {
       throw new IllegalArgumentException("Invalid jump position.");
     }
-    this.to = to;
+    this.to = inputTo;
   }
 }
