46224a5771d5da1f8617c4cb5a0b51c87ba169f5
Tinius Presterud <github@tinius.dev>
2025-04-25 17:20:04 +0200

    Add UI consts

---
+++ src/main/java/constants/UiConstants.java
@@ -0,0 +1,29 @@
+package constants;
+
+public final class UiConstants {
+  // Main settings
+  public static final String APP_NAME =  "IDATG2001";
+
+  // Size settings
+  public static final Integer APP_WIDTH = 600;
+  public static final Integer APP_HEIGHT = 400;
+
+  // CSS Files
+  public static final String MAIN_MENU_CSS = "/style/menu.css";
+
+  // Game settings
+  public static final String SNAKES_LADDERS = "Snakes & Ladders";
+  public static final String TIC_TAC_TOE = "Tic-Tac-Toe";
+  public static final String CONNECT_FOUR = "Connect Four";
+
+  // Naming
+  public static final String MAIN_MENU = "Main Menu";
+  public static final String EXIT = "Exit";
+
+  // Error handling
+  public static boolean PRINT_STACK_TRACE = false;
+
+  // Menu Styling
+  public static final Integer MENU_VBox_SPACING = 15;
+  public static final Integer MENU_BUTTON_PADDING = 40;
+}
