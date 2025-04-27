a6cf76eea679d3ebfe482a8a3ed1982e2da56ded
xyfuss <jonasaatomren@gmail.com>
2025-04-27 19:05:32 +0200

    add SettingsMenu

---
+++ src/main/java/ui/SettingsMenu.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SettingsMenu extends Application {
+  @Override
+  public void start(Stage stage) throws Exception {
+
+  }
+}
