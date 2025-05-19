50a853c99e499256c387344591e9464ca74295b7
Tinius Presterud <github@tinius.dev>
2025-05-19 11:13:12 +0200

    Add TTT UI package-info.java

---
+++ src/main/java/tictactoe/ui/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the classes and interfaces for the Tic-tac-toe user interface.
+ * This package contains the components required to create and manage
+ * the graphical user interface for the Tic-tac-toe game.
+ *
+ * @author tiniuspre
+ * @version 19.05.2025
+ * @since 25.03.2025
+ */
+package tictactoe.ui;
