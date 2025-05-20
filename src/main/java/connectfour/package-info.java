5c48414189702c993bca3f8f5d48a3dc5c9a5844
Tinius Presterud <github@tinius.dev>
2025-05-20 15:59:41 +0200

    update package-info for connectfour package with detailed description

--- src/main/java/connectfour/package-info.java
+++ src/main/java/connectfour/package-info.java
@@ -1,1 +1,14 @@
+/**
+ * Root package for the Connect Four application.
+ * <p>
+ * This package contains all modules
+ * and classes related to the Connect Four game,
+ * including the game engine,
+ * user interface, and supporting utilities.
+ * </p>
+ *
+ * @author tiniuspre
+ * @version 19.05.2025
+ * @since 19.05.2025
+ */
 package connectfour;
